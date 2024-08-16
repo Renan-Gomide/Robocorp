651f0f79c16f869bec9203481ff8ebcb6ed122c3
Renan Gomide <renangomide@gmail.com>
2024-08-16 18:38:58 -0300

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,3 +1,16 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")  # Se quiser rodar em modo headless
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+
+
+
+
+#####
 from robocorp.tasks import task
 import os
 import re
